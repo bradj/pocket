4f4e2f5a49f2a51f6838227f8307cd3922b4eb7c
bradj <brad@bradjanke.com>
2021-02-07 19:18:48 -0600

    Adds basic auth with jwt

--- pocket/model/user.py
+++ pocket/model/user.py
@@ -1,7 +1,17 @@
-from mongoengine import StringField, Document
+from typing import Optional
+
+from mongoengine import StringField, Document, BooleanField
+from pydantic import BaseModel
 
 
-class User(Document):
+class UserDB(Document):
     email = StringField(required=True, max_length=64)
     username = StringField(required=True, max_length=30)
     password = StringField(required=True, max_length=128)
+    disabled = BooleanField(default=False)
+
+
+class User(BaseModel):
+    email: str
+    username: str
+    disabled: Optional[bool] = False
